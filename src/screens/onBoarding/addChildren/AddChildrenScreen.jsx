8f2cad20b4e03c0fd4aa866135d641a61da8f498
Saadin <hyphensaad@gmail.com>
2023-07-24 14:41:33 +0500

    fixed scaling of add children screen

--- src/screens/onBoarding/addChildren/AddChildrenScreen.jsx
+++ src/screens/onBoarding/addChildren/AddChildrenScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import { alpha, useTheme } from '@mui/material/styles'
 import { useNavigate } from 'react-router-dom'
 
@@ -17,9 +17,10 @@ import {
   RightArrowIcon,
 } from '../../../icons'
 
-import { tokens } from '../../../theme'
 import { ASSETS } from '../../../config/assets'
 import { ROUTES } from '../../../config/routes'
+import { tokens } from '../../../theme'
+import { $ } from '../../../utils'
 
 const ChildInfoCard = ({
   fullname = 'Child Name',
@@ -52,105 +53,108 @@ const ChildInfoCard = ({
     <Box sx={{
       display: 'flex',
       flexDirection: 'column',
-      gap: '24px',
+      gap: $({ size: 24 }),
       alignItems: 'center',
     }}>
       <Box sx={{
-        borderRadius: '24px',
+        borderRadius: $({ size: 24 }),
         backgroundColor: backgroundColor,
-        padding: '16px 24px',
+        padding: `${$({ size: 16 })} ${$({ size: 24 })}`,
         display: 'flex',
-        minWidth: '350px',
+        maxWidth: $({ size: 450 }),
+        width: $({ size: 320 }),
         position: 'relative',
       }}>
         {
           hasInfo &&
-          <Box component='img' src={ASSETS.ON_BOARDING.CHILD} sx={{
-            width: '110px',
-            height: '110px',
-            borderRadius: '110px',
-            marginRight: '24px',
-          }} />
+          <img
+            alt='child'
+            src={ASSETS.ON_BOARDING.CHILD}
+            style={{
+              width: $({ size: 80 }),
+              height: $({ size: 80 }),
+              borderRadius: $({ size: 80 }),
+              marginRight: $({ size: 16 }),
+            }} />
         }
 
         {
           !hasInfo &&
           <Box sx={{
-            width: '110px',
-            height: '110px',
-            borderRadius: '110px',
-            marginRight: '24px',
+            width: $({ size: 80 }),
+            height: $({ size: 80 }),
+            borderRadius: $({ size: 80 }),
+            marginRight: $({ size: 16 }),
             backgroundColor: colors.white[800],
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-          }}><PersonIcon size={50} color={disabled ? colors.extra.grey3 : colors.grey[100]} /></Box>
+          }}><PersonIcon
+              size={$({ size: 50, numeric: true })}
+              color={disabled ? colors.extra.grey3 : colors.grey[100]} />
+          </Box>
         }
 
         {
           disabled &&
-          <Box sx={{
-            position: 'absolute',
-            top: '20px',
-            right: '20px',
-          }}>
+          <Box sx={{ position: 'absolute', top: $({ size: 16 }), right: $({ size: 16 }) }}>
             <LockIcon />
           </Box>
         }
 
         <Box>
           <Typography sx={{
-            fontSize: '18px',
+            fontSize: $({ size: 18 }),
             fontWeight: '600',
-            lineHeight: '30px',
+            lineHeight: $({ size: 30 }),
             color: detailColor,
-            marginBottom: '8px',
+            marginBottom: $({ size: 8 }),
           }}>{disabled ? 'Add Child' : fullname}</Typography>
 
           <Box sx={{ display: 'flex' }}>
             <Typography sx={{
-              fontSize: '13.5px',
+              fontSize: $({ size: 13.5 }),
               fontWeight: '400',
-              lineHeight: '25px',
+              lineHeight: $({ size: 25 }),
               color: detailHeadingColor
             }}>Age:&nbsp;</Typography>
 
             <Typography sx={{
-              fontSize: '13.5px',
+              fontSize: $({ size: 13.5 }),
               fontWeight: '600',
-              lineHeight: '25px',
+              lineHeight: $({ size: 25 }),
               color: detailColor,
             }}>{age}</Typography>
           </Box>
 
           <Box sx={{ display: 'flex' }}>
             <Typography sx={{
-              fontSize: '13.5px',
+              fontSize: $({ size: 13.5 }),
               fontWeight: '400',
-              lineHeight: '25px',
+              lineHeight: $({ size: 25 }),
               color: detailHeadingColor
             }}>Gender:&nbsp;</Typography>
 
             <Typography sx={{
-              fontSize: '13.5px',
+              fontSize: $({ size: 13.5 }),
               fontWeight: '600',
-              lineHeight: '25px',
+              lineHeight: $({ size: 25 }),
               color: detailColor,
             }}>{gender}</Typography>
           </Box>
 
           <Box sx={{ display: 'flex' }}>
             <Typography sx={{
-              fontSize: '13.5px',
+              fontSize: $({ size: 13.5 }),
               fontWeight: '400',
-              lineHeight: '25px',
+              lineHeight: $({ size: 25 }),
               color: detailHeadingColor
             }}>Difficulty:&nbsp;</Typography>
 
             <Typography sx={{
-              fontSize: '13.5px',
+              fontSize: $({ size: 13.5 }),
               fontWeight: '600',
-              lineHeight: '25px',
+              lineHeight: $({ size: 25 }),
               color: detailColor,
             }}>{difficulty}</Typography>
           </Box>
@@ -159,13 +163,13 @@ const ChildInfoCard = ({
 
       {
         hasInfo &&
-        <Box sx={{ display: 'flex', gap: '24px' }}>
-          <Box component='span' onClick={() => { alert('Coming Soon...') }}>
-            <TrashIcon />
-          </Box>
-          <Box component='span' onClick={() => { alert('Coming Soon...') }}>
-            <EditIcon />
-          </Box>
+        <Box sx={{ display: 'flex', gap: $({ size: 24 }) }}>
+          <span onClick={() => { alert('Coming Soon...') }}>
+            <TrashIcon size={$({ size: 32, numeric: true })} />
+          </span>
+          <span onClick={() => { alert('Coming Soon...') }}>
+            <EditIcon size={$({ size: 32, numeric: true })} />
+          </span>
         </Box>
       }
 
@@ -179,6 +183,8 @@ const ChildInfoCard = ({
             '&:hover': {
               backgroundColor: alpha(colors.extra.grey1, 0.8),
             },
+            maxWidth: $({ size: 450 }),
+            width: $({ size: 320 }),
           }}
         />
       }
@@ -200,17 +206,18 @@ const AddChildrenScreen = () => {
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
-      padding: '2.5%',
+      padding: $({ size: 40 }),
     }}>
       <Box sx={{
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'space-between',
         backgroundColor: colors.white[800],
-        boxShadow: `0px 0px 8px 0px ${alpha(colors.solids.black, 0.25)}`,
+        boxShadow: `0 0 ${$({ size: 8 })} 0 ${alpha(colors.solids.black, 0.25)}`,
         width: '100%',
-        borderRadius: '12px',
+        borderRadius: $({ size: 12 }),
         flexGrow: 1,
+        gap: $({ size: 24 }),
       }}>
         <Box sx={{
           display: 'flex',
@@ -220,16 +227,16 @@ const AddChildrenScreen = () => {
           height: '100%',
         }}>
           <Box component='img' src={ASSETS.LOGO} sx={{
-            width: '200px',
+            width: $({ size: 160 }),
             alignSelf: 'flex-start',
-            margin: '40px 0px 0px 40px',
+            margin: `${$({ size: 40 })} 0 0 ${$({ size: 40 })}`
           }} />
 
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Box sx={{
-              borderRadius: '150px',
+              borderRadius: $({ size: 160 }),
               backgroundColor: colors.extra.iconBackground,
-              padding: '48px',
+              padding: $({ size: 48 }),
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
@@ -237,27 +244,42 @@ const AddChildrenScreen = () => {
             }}><ChildrenGroupIcon /></Box>
 
             <Typography sx={{
-              fontSize: '32px',
+              fontSize: $({ size: 32 }),
               fontWeight: '600',
-              lineHeight: '40px',
+              lineHeight: $({ size: 40 }),
               textAlign: 'center',
               color: colors.grey[200],
-              margin: '24px 0px',
+              margin: `${$({ size: 24 })} 0`,
             }}>Your Children</Typography>
           </Box>
 
-          <Box sx={{ display: 'flex', gap: '24px' }}>
-            <ChildInfoCard fullname='Eddie Johnson' age='08' gender='Male' difficulty='Medium' />
-            <ChildInfoCard hasInfo={false} />
-            <ChildInfoCard hasInfo={false} disabled={true} />
-          </Box>
+          <Grid container sx={{
+            minWidth: $({ size: 320 }),
+            width: $({ size: (320 * 3) + (24 * 2) }),
+            maxWidth: '100%',
+            rowGap: $({ size: 24 }),
+          }}>
+            <Grid item xs={12} md={6} lg={4}>
+              <ChildInfoCard fullname='Eddie Johnson' age='08' gender='Male' difficulty='Medium' />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <ChildInfoCard hasInfo={false} />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <ChildInfoCard hasInfo={false} disabled={true} />
+            </Grid>
+          </Grid>
         </Box>
 
         <CustomButton
           label='Continue'
           rightIcon={<RightArrowIcon />}
-          sx={{ width: 'fit-content', marginBottom: '40px', marginRight: '40px', alignSelf: 'flex-end' }}
-          rightIconSx={{ marginLeft: '16px' }}
+          sx={{
+            width: 'fit-content',
+            marginBottom: $({ size: 40 }),
+            marginRight: $({ size: 40 }),
+            alignSelf: 'flex-end'
+          }}
           onClick={() => {
             navigate(ROUTES.ON_BOARDING.PERSONALITY_TESTS)
           }}
